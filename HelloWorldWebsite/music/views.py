8a81c9bf7003854de08d4f78082d403f88a86bb6
Arshad Mohammed <ararmoha@oradev.oraclecorp.com>
2016-07-05 00:44:40 +0530

    Returning 404 when album not found, added detail.html template for testing

--- HelloWorldWebsite/music/views.py
+++ HelloWorldWebsite/music/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from .models import Album
 from django.template import loader
 
 # Create your views here.
 def hello(request):
     all_albums = Album.objects.all()
-    mytemplate = loader.get_template('music/index.html')
-    context = {
-        'all_albs' : all_albums
-    }
+    # mytemplate = loader.get_template('music/index.html')
+    # context = {
+    #     'all_albs' : all_albums
+    # }
+    # return HttpResponse(mytemplate.render(context,request))
 
-    return HttpResponse(mytemplate.render(context,request))
+    #Instead of above , we can use render() function which is lot cleaner
+    context = { 'all_albs' : all_albums }
+    return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
-    return HttpResponse("<h1>The selected album is : " + album_id)
+    try:
+        curr_album = Album.objects.get(pk=album_id)
+    except:
+        raise Http404("Oops! Not Found ! Better luck next time.") #returning 404 when no such album exists
+        
+    return render(request, "music/detail.html", { 'album_title' : curr_album.album_title })
