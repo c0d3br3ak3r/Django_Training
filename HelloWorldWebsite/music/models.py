e19c1c51a38af04020f2da785cc3e8ad831dbf80
Arshad Mohammed <ararmoha@oradev.oraclecorp.com>
2016-07-04 23:46:39 +0530

    Changes for adding Album,Song tables to the database\n Registering the db table Album into admin page

--- HelloWorldWebsite/music/models.py
+++ HelloWorldWebsite/music/models.py
@@ -5,6 +5,8 @@ class Album(models.Model):
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
+    def __str__(self):
+        return self.artist + ' ' + self.album_title
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
