fbd4acda29a1522b91c8e0d0d825736ae4c37d5d
Arshad Mohammed <ararmoha@oradev.oraclecorp.com>
2016-07-05 00:23:05 +0530

    Using templates

--- HelloWorldWebsite/music/views.py
+++ HelloWorldWebsite/music/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Album
+from django.template import loader
 
 # Create your views here.
 def hello(request):
-    html = ''
     all_albums = Album.objects.all()
-    for alb in all_albums:
-        url = '/music/' + str(alb.id) + '/'
-        html += '<a href="' + url + '">' + alb.album_title +'</a>'
-    return HttpResponse(html)
+    mytemplate = loader.get_template('music/index.html')
+    context = {
+        'all_albs' : all_albums
+    }
+
+    return HttpResponse(mytemplate.render(context,request))
 
 def detail(request, album_id):
     return HttpResponse("<h1>The selected album is : " + album_id)
