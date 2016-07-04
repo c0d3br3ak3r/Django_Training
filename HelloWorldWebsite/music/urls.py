b192d2bc1e553c76a2eac8fe271c680cd39f6b48
Arshad Mohammed <ararmoha@oradev.oraclecorp.com>
2016-07-05 00:12:13 +0530

    changed index function to point to all albums on hitting music/ url

--- HelloWorldWebsite/music/urls.py
+++ HelloWorldWebsite/music/urls.py
@@ -4,5 +4,10 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    # /music/
     url(r'^$', views.hello, name='helloname'),
+
+    #/music/id
+    url(r'^(?P<album_id>[0-9]*)/$', views.detail, name='detail' ) #Note that the / is required at the end.
+
 ]
--- HelloWorldWebsite/music/views.py
+++ HelloWorldWebsite/music/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album
 
 # Create your views here.
 def hello(request):
-    return HttpResponse("<h1>Hello world! This is my first django app</h1>")
+    html = ''
+    all_albums = Album.objects.all()
+    for alb in all_albums:
+        url = '/music/' + str(alb.id) + '/'
+        html += '<a href="' + url + '">' + alb.album_title +'</a>'
+    return HttpResponse(html)
+
+def detail(request, album_id):
+    return HttpResponse("<h1>The selected album is : " + album_id)
